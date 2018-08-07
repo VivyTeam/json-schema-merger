d69e42b4e867e0508ff3a21323a4dbf52e6fa48c
Kirill Merkushev <lanwen@yandex.ru>
2018-08-07 22:04:32 +0200

    allow multiple sources for events

--- src/main/java/io/vivy/JsonSchemaMerger.java
+++ src/main/java/io/vivy/JsonSchemaMerger.java
@@ -7,16 +7,17 @@ import me.andrz.jackson.JsonReferenceProcessor;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class JsonSchemaMerger {
 
     private static ObjectMapper JSON_MAPPER = new ObjectMapper();
 
-    public static File mergeSchemasToFile(Path targetDir, Path schemasDir) {
+    public static File mergeSchemasToFile(Path targetDir, Path... schemasDir) {
         return mergeSchemasToPath(targetDir, schemasDir).toFile();
     }
 
-    public static Path mergeSchemasToPath(Path targetDir, Path schemasDir) {
+    public static Path mergeSchemasToPath(Path targetDir, Path... schemasDir) {
         try {
             Path schemaFile = Files.createTempFile(targetDir, "schema", ".json");
 
@@ -26,34 +27,43 @@ public class JsonSchemaMerger {
         }
     }
 
-    public static byte[] mergeSchemas(Path schemasDir) {
+    public static byte[] mergeSchemas(Path... schemasDirectories) {
         try {
             JsonReferenceProcessor jsonReferenceProcessor = new JsonReferenceProcessor();
 
             ObjectNode schemaNode = JSON_MAPPER.createObjectNode();
             ObjectNode eventsNode = schemaNode.with("events");
-            Files.walk(schemasDir)
-                    .filter(Files::isRegularFile)
-                    .filter(it -> !it.getFileName().toString().startsWith("_")) // Skip defs
-                    .forEach(it -> {
-                        try {
-                            String filePath = schemasDir.relativize(it).toString();
-                            ObjectNode eventNode = (ObjectNode) jsonReferenceProcessor.process(it.toFile());
 
-                            // Defaults
-                            ObjectNode propertiesNode = eventNode.with("properties");
-                            propertiesNode.with("eventId").put("type", "string");
-                            propertiesNode.with("eventType").put("type", "string");
+            for (Path baseDir : schemasDirectories) {
+                try (Stream<Path> pathsStream = Files.walk(baseDir)) {
+                    pathsStream
+                            .filter(Files::isRegularFile)
+                            .filter(it -> !it.getFileName().toString().startsWith("_")) // Skip defs
+                            .forEach(schemaFile -> {
+                                try {
+                                    ObjectNode eventNode = (ObjectNode) jsonReferenceProcessor.process(schemaFile.toFile());
 
-                            eventsNode.set(filePath.substring(0, filePath.lastIndexOf(".")), eventNode);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
+                                    // Defaults
+                                    ObjectNode propertiesNode = eventNode.with("properties");
+                                    propertiesNode.with("eventId").put("type", "string");
+                                    propertiesNode.with("eventType").put("type", "string");
+
+                                    eventsNode.set(asType(baseDir, schemaFile), eventNode);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                }
+            }
 
             return JSON_MAPPER.writeValueAsBytes(schemaNode);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String asType(Path parent, Path filePath) {
+        String relativePath = parent.relativize(filePath).toString();
+        return relativePath.substring(0, relativePath.lastIndexOf("."));
     }
 }
